eae3eaf2eebca1b2ebc3da5634e496e2fa4f3d0f
manvi verma <manviverma307@gmail.com>
2023-03-01 13:45:50 +0530

    latest todo code

--- src/Pages/Todo/Todo.js
+++ src/Pages/Todo/Todo.js
@@ -1,70 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Todolist } from "./Todolist";
+import "./Todo.css";
 
-
-
-import React, { useState,useEffect } from 'react';
-import "./Todo.css"
-import { Todolist } from './Todolist';
-
-export const Todo=()=> {
+export const Todo = () => {
   const LOCAL_STORAGE_KEY = "todos";
-    const [todos, setTodos] = useState(()=>{
-      if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
-        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if (storedTodos) return storedTodos;
-      }else{
-        return []
-      }
-    });
-    const [newTodo, setNewTodo] = useState('');
-   
-   
-    useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
+  const [todos, setTodos] = useState(() => {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+      const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      if (storedTodos) return storedTodos;
+    } else {
+      return [];
+    }
+  });
+  const [newTodo, setNewTodo] = useState("");
 
-    useEffect(() => {
-      if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
-        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if (storedTodos) setTodos(storedTodos);
-      }
-      
-    }, []);
- 
-
-  
-
-    const handleAddTodo = () => {
-      const todo = { title: newTodo, completed: false };
-      setTodos([...todos, todo]);
-      setNewTodo('');
-    };
-  
-   
-  
-    const handleClearCompleted = () => {
-      const newTodos = todos.filter((todo) => !todo.completed);
-      setTodos(newTodos);
-    };
-  
-    return (
-      <div className='container'>
-      <div className="todo-list-container">
-      <h1>Todo List</h1>
-      <div className="add-todo-container">
-        <input value={newTodo} onChange={(e)=>setNewTodo(e.target.value)} />
-        <button onClick={handleAddTodo}>Add</button>
-      </div>
-      <ul className="todo-list">
-        {todos.map((todo, index) => (
-          <Todolist todos={todos}  setTodos={setTodos} index={index} todo={todo} />
-        ))}
-      </ul>
-      <button className="clear-completed" onClick={handleClearCompleted}>
-        Clear Completed Todo's
-      </button>
-    </div>
-    </div>
-    );
+  // for adding the todo's
+  const handleAddTodo = () => {
+    const todo = { id: todos.length, title: newTodo, completed: false };
+    setTodos([...todos, todo]);
+    setNewTodo("");
   };
 
+  const handleClearCompleted = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+      const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      if (storedTodos) setTodos(storedTodos);
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="todo-list-container">
+        <h1>Todo List</h1>
+        <div className="add-todo-container">
+          <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+          <button onClick={handleAddTodo}>Add</button>
+        </div>
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <Todolist
+              todos={todos}
+              setTodos={setTodos}
+              index={index}
+              todo={todo}
+              setNewTodo={setNewTodo}
+              newTodo={newTodo}
+            />
+          ))}
+        </ul>
+        <button className="clear-completed" onClick={handleClearCompleted}>
+          Clear Completed Todo's
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Todo/Todolist.js
+++ src/Pages/Todo/Todolist.js
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 
 export const Todolist = (props) => {
   const [isEdit, setIsEdit] = useState(false);
+  const [updatedTodo, setupdatedTodo] = useState("");
 
+  // for completing the todo's
   const handleToggleCompleted = (index) => {
     const newTodos = [...props?.todos];
     newTodos[index].completed = !newTodos[index].completed;
     props?.setTodos(newTodos);
   };
 
+  // for editing the todo's
   const handleEditTodo = (index, newTitle) => {
     const newTodos = [...props?.todos];
-    console.log(newTodos, "newTodos");
     newTodos[index].title = newTitle;
     props?.setTodos(newTodos);
+    setIsEdit(false);
+  };
+
+  const handleChange = (e) => {
+    setupdatedTodo(e.target.value);
+  };
+  const onSaveEditTodos = (e) => {
+    let updateValue = props?.todos?.map((item) => {
+      return e.target.id == item.id
+        ? { ...item, title: updatedTodo }
+        : { ...item };
+    });
+
+    props?.setTodos(updateValue);
     setIsEdit(false);
   };
 
@@ -30,9 +46,22 @@ export const Todolist = (props) => {
         />
         {props?.todo.completed ? (
           props?.todo.title
+        ) : isEdit ? (
+          <div className="add-todo-container">
+            <input
+              contentEditable={isEdit}
+              defaultValue={props?.todo.title}
+              onChange={(e) => handleChange(e)}
+            />{" "}
+            <span>
+              <button id={props?.todo?.id} onClick={(e) => onSaveEditTodos(e)}>
+                Save Edited Todo
+              </button>
+            </span>
+          </div>
         ) : (
           <span
-            contentEditable={isEdit}
+            // contentEditable={isEdit}
             onDoubleClick={(e) => setIsEdit(true)}
             onBlur={(e) => handleEditTodo(props?.index, e.target.textContent)}
           >
